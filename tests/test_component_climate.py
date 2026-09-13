33d80153b3052f955b3a355054affc0468110c5a
Dino del Favero <dino@mesina.net>
2026-09-13 16:22:16 +0200

    Update test_component_climate.py

--- tests/test_component_climate.py
+++ tests/test_component_climate.py
@@ -641,4 +641,5 @@ async def test_climate_knob_positions_coverage(hass):
     
     mock_event.local_offset = 2
     mock_event.local_control_state = LOCAL_CONTROL_OFFSET
+    entity.handle_event(mock_event)
     assert entity._knob_pos == "+2"
